54729c9cb78b469900b3596b92b13552dbad8f03
Pavel Ulyanov <ulyanovproff@gmail.com>
2023-09-13 22:47:05 +0500

    go sleep after

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 100;
+        int third = 1000;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
+
 }
--- src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
+++ src/main/java/school/mjc/stage0/module3/task3/ReassigningValues.java
@@ -2,6 +2,29 @@ package school.mjc.stage0.module3.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 100;
+        int third = 1000;
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+
+        int linkToFirst = first;
+        int linkToSecond = second;
+        int linkToThird = third;
+
+        first = 15;
+        second = 6;
+        third = 4;
+
+        System.out.println(linkToFirst);
+        System.out.println(linkToSecond);
+        System.out.println(linkToThird);
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
     }
 }
