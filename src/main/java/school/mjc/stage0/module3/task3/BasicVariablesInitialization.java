b4aa4ee089d55ec4b280dd026d5c7669faabd41e
Pavel Ulyanov <ulyanovproff@gmail.com>
2023-09-14 10:03:20 +0500

    final, it's true

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -3,8 +3,8 @@ package school.mjc.stage0.module3.task3;
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
         int first = 1;
-        int second = 100;
-        int third = 1000;
+        int second = 10;
+        int third = 100;
 
         System.out.println(first);
         System.out.println(second);
